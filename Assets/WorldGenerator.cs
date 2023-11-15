7d2245fb52aa2c08e785c65a69133bcc292f3ea6
Gerard Smit <gsmit1996@gmail.com>
2023-11-15 23:52:35 +0100

    feat: world generation in a seperate thread

--- Assets/WorldGenerator.cs
+++ Assets/WorldGenerator.cs
@@ -1,10 +1,13 @@
 using System;
+using System.Threading;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class WorldGenerator : MonoBehaviour
 {
     public int WorldSize = 10;
+
+    private World world = new();
 
     private static readonly BlockProperties testBlock = new()
     {
@@ -59,112 +62,144 @@ public class WorldGenerator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateMeshTest();
+        var thread = new Thread(CreateMeshTest)
+        {
+            IsBackground = true
+        };
+
+        thread.Start();
+    }
+
+    public void Update()
+    {
+        if (world.DirtyChunks.TryDequeue(out var chunk))
+        {
+            UpdateChunkMesh(chunk);
+        }
+    }
+
+    public void GenerateChunk(Chunk chunk)
+    {
+        chunk.Generating = true;
+
+        var chunkX = chunk.Position.x;
+        var chunkY = chunk.Position.y;
+        var random = new System.Random();
+
+        for (var blockX = 0; blockX < 16; blockX++)
+        {
+            for (var blockZ = 0; blockZ < 16; blockZ++)
+            {
+                var height = 48f + Mathf.PerlinNoise(chunkX * 16 + blockX * 0.05333f, chunkY * 16 + blockZ * 0.05333f) * 8f;
+                var terrainHeight = Mathf.RoundToInt(height);
+
+                for (var blockY = 0; blockY < terrainHeight; blockY++)
+                {
+                    chunk.SetBlock(blockX, blockY, blockZ,
+                        blockY == terrainHeight - 1
+                        ? grassBlock
+                        : blockY < terrainHeight - (2 + random.Next(1, 3))
+                            ? random.Next(1, 12) == 10 ? coalBlock : stoneBlock
+                            : dirtBlock);
+                }
+
+                var treeChance = random.Next(1, 100);
+                if (treeChance < 4)
+                {
+                    var treeStemHeight = random.Next(3, 8);
+                    for (var treeStemY = 0; treeStemY < treeStemHeight; treeStemY++)
+                    {
+                        chunk.SetBlock(blockX, terrainHeight + treeStemY, blockZ, new BlockProperties
+                        {
+                            TileTop = 5,
+                            TileBottom = 5,
+                            TileLeft = 6,
+                            TileRight = 6,
+                            TileFront = 6,
+                            TileBack = 6
+                        });
+                    }
+
+                    var treeTopHeight = random.Next(2, 4);
+                    // create a spherical tree top
+                    for (var treeTopX = -treeTopHeight; treeTopX <= treeTopHeight; treeTopX++)
+                    {
+                        for (var treeTopY = 0; treeTopY <= treeTopHeight; treeTopY++)
+                        {
+                            for (var treeTopZ = -treeTopHeight; treeTopZ <= treeTopHeight; treeTopZ++)
+                            {
+                                if (treeTopX * treeTopX + treeTopY * treeTopY + treeTopZ * treeTopZ <= treeTopHeight * treeTopHeight)
+                                {
+                                    var leafX = blockX + treeTopX;
+                                    var leafZ = blockZ + treeTopZ;
+                                    if (leafX < 0 || leafX >= 15 || leafZ < 0 || leafZ >= 15)
+                                    {
+                                        continue;
+                                    }
+
+                                    chunk.SetBlock(leafX, terrainHeight + treeStemHeight + treeTopY, leafZ, new BlockProperties
+                                    {
+                                        TileTop = 7,
+                                        TileBottom = 7,
+                                        TileLeft = 7,
+                                        TileRight = 7,
+                                        TileFront = 7,
+                                        TileBack = 7
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        chunk.Generating = false;
+        chunk.SetDirty();
+    }
+
+    public void UpdateChunkMesh(Chunk chunk)
+    {
+        var chunkX = chunk.Position.x;
+        var chunkY = chunk.Position.y;
+
+        if (chunk.ChunkAccessor != null)
+        {
+            chunk.ChunkAccessor.UpdateMesh();
+            return;
+        }
+
+        var gameObject = new GameObject();
+        gameObject.name = $"Chunk {chunkX}, {chunkY}";
+        gameObject.transform.parent = transform;
+        gameObject.transform.position = new Vector3(chunkX * 16, 0, chunkY * 16);
+
+        gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshCollider>();
+
+        var meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        var mat = Resources.Load<Material>("Materials/Block");
+        mat.mainTexture = Resources.Load<Texture2D>("Sprites/world");
+        meshRenderer.material = mat;
+
+        var chunkAccessor = gameObject.AddComponent<ChunkAccessor>();
+        chunkAccessor.Init(chunk);
+
+        chunk.ChunkAccessor = chunkAccessor;
     }
 
     public void CreateMeshTest()
     {
         var chunksX = WorldSize;
         var chunksY = WorldSize;
-        
-        var world = new World();
+
         for (var chunkX = 0; chunkX < chunksX; chunkX++)
         {
             for (var chunkY = 0; chunkY < chunksY; chunkY++)
             {
                 var chunk = world.RequestChunk(chunkX, chunkY);
-                for (var blockX = 0; blockX < 16; blockX++)
-                {
-                    for (var blockZ = 0; blockZ < 16; blockZ++)
-                    {
-                        var height = 48f + Mathf.PerlinNoise(chunkX * 16 + blockX * 0.05333f, chunkY * 16 + blockZ * 0.05333f) * 8f;
-                        var terrainHeight = Mathf.RoundToInt(height);
 
-                        for (var blockY = 0; blockY < terrainHeight; blockY++)
-                        {
-                            chunk.SetBlock(blockX, blockY, blockZ,
-                                blockY == terrainHeight - 1 
-                                ? grassBlock 
-                                : blockY < terrainHeight - (2 + Random.Range(1, 3))
-                                    ? Random.Range(1, 12) == 10 ? coalBlock : stoneBlock  
-                                    : dirtBlock);
-                        }
-                        
-                        var treeChance = Random.Range(1, 100);
-                        if (treeChance < 4)
-                        {
-                            var treeStemHeight = Random.Range(3, 8);
-                            for (var treeStemY = 0; treeStemY < treeStemHeight; treeStemY++)
-                            {
-                                chunk.SetBlock(blockX, terrainHeight + treeStemY, blockZ, new BlockProperties
-                                {
-                                    TileTop = 5,
-                                    TileBottom = 5,
-                                    TileLeft = 6,
-                                    TileRight = 6,
-                                    TileFront = 6,
-                                    TileBack = 6
-                                });
-                            }
-                                
-                            var treeTopHeight = Random.Range(2, 4);
-                            // create a spherical tree top
-                            for (var treeTopX = -treeTopHeight; treeTopX <= treeTopHeight; treeTopX++)
-                            {
-                                for (var treeTopY = 0; treeTopY <= treeTopHeight; treeTopY++)
-                                {
-                                    for (var treeTopZ = -treeTopHeight; treeTopZ <= treeTopHeight; treeTopZ++)
-                                    {
-                                        if (treeTopX * treeTopX + treeTopY * treeTopY + treeTopZ * treeTopZ <= treeTopHeight * treeTopHeight)
-                                        {
-                                            var leafX = blockX + treeTopX;
-                                            var leafZ = blockZ + treeTopZ;
-                                            if (leafX < 0 || leafX >= 15 || leafZ < 0 || leafZ >= 15)
-                                            {
-                                                continue;
-                                            }
-                        
-                                            chunk.SetBlock(leafX, terrainHeight + treeStemHeight + treeTopY, leafZ, new BlockProperties
-                                            {
-                                                TileTop = 7,
-                                                TileBottom = 7,
-                                                TileLeft = 7,
-                                                TileRight = 7,
-                                                TileFront = 7,
-                                                TileBack = 7
-                                            });
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                var gameObject = new GameObject();
-                gameObject.transform.position = new Vector3(chunkX * 16, 0, chunkY * 16);
-                var meshFilter = gameObject.AddComponent<MeshFilter>();
-                var meshRenderer = gameObject.AddComponent<MeshRenderer>();
-                var meshCollider = gameObject.AddComponent<MeshCollider>();
-                var chunkAccessor = gameObject.AddComponent<ChunkAccessor>();
-
-                chunkAccessor.Init(chunk);
-                
-                var mat = Resources.Load<Material>("Materials/Block");
-                mat.mainTexture = Resources.Load<Texture2D>("Sprites/world");
-
-                meshFilter.mesh = chunk.GetMesh();
-                meshCollider.sharedMesh = meshFilter.mesh;
-                meshRenderer.material = mat;
-            }
-        }
-        
-        for (var chunkX = 0; chunkX < chunksX; chunkX++)
-        {
-            for (var chunkY = 0; chunkY < chunksY; chunkY++)
-            {
-                var chunk = world.GetChunk(chunkX, chunkY);
-                chunk.SetDirty();
+                GenerateChunk(chunk);
             }
         }
     }
